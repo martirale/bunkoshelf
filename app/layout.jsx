685bd9a224a7bd3a36ccf3148b30e01361772a93
martirale <hola@alemartir.com>
2025-05-02 23:53:29 -0600

    Remove default files

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Bunko Shelf",
+  description: "Lorem ipsum dolor sit amet.",
+};
+
+export default function RootLayout({ children }) {
+  return <html lang="es">{children}</html>;
+}
