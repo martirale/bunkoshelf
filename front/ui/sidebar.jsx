9f0ffd7758c64927760ee0d896100a2302945de0
martirale <hola@alemartir.com>
2025-05-03 12:53:17 -0600

    Customize sidebar nav

--- front/ui/sidebar.jsx
+++ front/ui/sidebar.jsx
@@ -1,4 +1,8 @@
 "use client";
+import { House } from "lucide-react";
+import { BookHeart } from "lucide-react";
+import { Menu } from "lucide-react";
+import { LibraryBig } from "lucide-react";
 import { LogOut } from "lucide-react";
 import { useState } from "react";
 
@@ -8,34 +12,46 @@ export default function Sidebar() {
   return (
     <>
       {/* Sidebar para escritorio */}
-      <aside className="hidden md:flex md:w-2/12 bg-onix flex-col justify-between p-4">
+      <aside className="hidden md:flex md:w-2/12 bg-blackamber flex-col justify-between p-4">
         <div>
           <h1 className="text-2xl font-bold">Bunko Shelf</h1>
         </div>
-        <nav className="mt-6 space-y-4 flex-1">
-          <a href="#" className="block hover:text-pearl">
+        <nav className="mt-6 space-y-4 flex-1 text-lg">
+          <a
+            href="/"
+            className="flex items-center px-4 py-3 border border-blackamber hover:border-lilah hover:bg-onix rounded-lg leading-none"
+          >
+            <House className="w-5 h-5 mr-2" />
             Inicio
           </a>
-          <a href="#" className="block hover:text-pearl">
+          <a
+            href="#"
+            className="flex items-center px-4 py-3 border border-blackamber hover:border-lilah hover:bg-onix rounded-lg leading-none"
+          >
+            <LibraryBig className="w-5 h-5 mr-2" />
             Biblioteca
           </a>
-          <a href="#" className="block hover:text-pearl">
+          <a
+            href="#"
+            className="flex items-center px-4 py-3 border border-blackamber hover:border-lilah hover:bg-onix rounded-lg leading-none"
+          >
+            <BookHeart className="w-5 h-5 mr-2" />
             Favoritos
           </a>
         </nav>
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center px-2">
           <p className="text-sm">v0.1.0</p>
 
           {/* Logout Button */}
           <button
-            className="hover:text-pearl"
+            className="border border-blackamber hover:border-lilah hover:text-pearl hover:bg-onix rounded-lg p-2 cursor-pointer"
             aria-label="Logout"
             onClick={() => {
               // AQUÍ LÓGICA CIERRE SESIÓN
               console.log("Logged out");
             }}
           >
-            <LogOut className="w-5 h05" />
+            <LogOut className="w-5 h-5" />
           </button>
         </div>
       </aside>
@@ -53,19 +69,43 @@ export default function Sidebar() {
             <div>
               <h1 className="text-2xl font-bold">Bunko Shelf</h1>
             </div>
-            <nav className="mt-6 space-y-4 flex-1">
-              <a href="#" className="block hover:text-pearl">
+            <nav className="mt-6 space-y-4 flex-1 text-lg">
+              <a
+                href="/"
+                className="flex items-center px-4 py-3 border border-onix hover:border-lilah hover:bg-onix rounded-lg leading-none"
+              >
+                <House className="w-5 h-5 mr-2" />
                 Inicio
               </a>
-              <a href="#" className="block hover:text-pearl">
+              <a
+                href="#"
+                className="flex items-center px-4 py-3 border border-onix hover:border-lilah hover:bg-onix rounded-lg leading-none"
+              >
+                <LibraryBig className="w-5 h-5 mr-2" />
                 Biblioteca
               </a>
-              <a href="#" className="block hover:text-pearl">
+              <a
+                href="#"
+                className="flex items-center px-4 py-3 border border-onix hover:border-lilah hover:bg-onix rounded-lg leading-none"
+              >
+                <BookHeart className="w-5 h-5 mr-2" />
                 Favoritos
               </a>
             </nav>
-            <div>
-              <p className="text-sm">itsmrtr</p>
+            <div className="flex justify-between items-center pl-4">
+              <p className="text-sm">v0.1.0</p>
+
+              {/* Logout Button */}
+              <button
+                className="border border-onix hover:border-lilah hover:text-pearl hover:bg-onix rounded-lg p-2 cursor-pointer"
+                aria-label="Logout"
+                onClick={() => {
+                  // AQUÍ LÓGICA CIERRE SESIÓN
+                  console.log("Logged out");
+                }}
+              >
+                <LogOut className="w-5 h-5" />
+              </button>
             </div>
           </aside>
         </div>
@@ -77,7 +117,7 @@ export default function Sidebar() {
         className="md:hidden absolute top-4 left-4 z-40 bg-onix p-2 rounded"
         aria-label="Abrir menú"
       >
-        &#9776;
+        <Menu className="w-7 h-7" />
       </button>
     </>
   );
