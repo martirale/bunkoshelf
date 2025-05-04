11ad0d8f56292600e64f531c11b6f05fd51e9046
martirale <hola@alemartir.com>
2025-05-03 23:02:08 -0600

    Fix switch logo path

--- front/ui/siebarLogo.jsx
+++ front/ui/siebarLogo.jsx
@@ -7,15 +7,18 @@ import { usePathname } from "next/navigation";
 export default function SidebarLogo() {
   const pathname = usePathname();
 
-  const isBooks = pathname.startsWith("/books");
+  const isBooks =
+    pathname.startsWith("/en/books") || pathname.startsWith("/es/books");
 
   const logoSrc = isBooks
     ? "/logos/BunkoShelfAsh.svg"
     : "/logos/BunkoShelfLilah.svg";
 
+  const lang = pathname.split("/")[1] === "en" ? "en" : "es";
+
   return (
     <div className="px-4">
-      <Link href="/" className="inline-block">
+      <Link href={`/${lang}`} className="inline-block">
         <Image
           src={logoSrc}
           alt="Bunko Shelf Logo"
