37198a64a69d43987dcedb93062c13dc095f4eb3
martirale <hola@alemartir.com>
2025-05-04 15:27:00 -0600

    Add sections

--- front/app/[lang]/books/page.jsx
+++ front/app/[lang]/books/page.jsx
@@ -5,10 +5,40 @@ export default async function BooksPage({ params }) {
   const intl = await getDictionary(lang);
 
   return (
-    <section className="w-full p-4 bg-ash">
-      <div className="pb-4">
-        <h2 className="text-sand">{intl.hero.keepreading}</h2>
-      </div>
-    </section>
+    <>
+      <section className="w-full p-4 bg-ash">
+        <div className="pb-4">
+          <h2 className="text-sand">{intl.hero.keepreading}</h2>
+        </div>
+      </section>
+
+      {/* IN PROGRESS */}
+      <section className="p-4">
+        <div className="pb-4">
+          <h2>{intl.libraries.inprogress}</h2>
+        </div>
+      </section>
+
+      {/* RECENTLY ADDED */}
+      <section className="p-4">
+        <div className="pb-4">
+          <h2>{intl.libraries.recentlyadded}</h2>
+        </div>
+      </section>
+
+      {/* RECENTLY UPDATED SERIES */}
+      <section className="p-4">
+        <div className="pb-4">
+          <h2>{intl.libraries.recentlyupdated}</h2>
+        </div>
+      </section>
+
+      {/* RECENTLY READ */}
+      <section className="p-4">
+        <div className="pb-4">
+          <h2>{intl.libraries.recentlyread}</h2>
+        </div>
+      </section>
+    </>
   );
 }
--- front/app/[lang]/page.jsx
+++ front/app/[lang]/page.jsx
@@ -5,10 +5,41 @@ export default async function HomePage({ params }) {
   const intl = await getDictionary(lang);
 
   return (
-    <section className="w-full p-4 bg-pearl">
-      <div className="pb-4">
-        <h2 className="text-onix">{intl.hero.keepreading}</h2>
-      </div>
-    </section>
+    <>
+      {/* HERO SECTION */}
+      <section className="p-4 bg-pearl">
+        <div className="pb-4">
+          <h2 className="text-onix">{intl.hero.keepreading}</h2>
+        </div>
+      </section>
+
+      {/* IN PROGRESS */}
+      <section className="p-4">
+        <div className="pb-4">
+          <h2>{intl.libraries.inprogress}</h2>
+        </div>
+      </section>
+
+      {/* RECENTLY ADDED */}
+      <section className="p-4">
+        <div className="pb-4">
+          <h2>{intl.libraries.recentlyadded}</h2>
+        </div>
+      </section>
+
+      {/* RECENTLY UPDATED SERIES */}
+      <section className="p-4">
+        <div className="pb-4">
+          <h2>{intl.libraries.recentlyupdated}</h2>
+        </div>
+      </section>
+
+      {/* RECENTLY READ */}
+      <section className="p-4">
+        <div className="pb-4">
+          <h2>{intl.libraries.recentlyread}</h2>
+        </div>
+      </section>
+    </>
   );
 }
