d9f9e14ea3d104786aa692a910fcd6f81de7eac9
martirale <hola@alemartir.com>
2025-05-04 23:09:04 -0600

    Restructure middleware

--- front/middleware.js
+++ front/middleware.js
@@ -1,46 +1,9 @@
-import { NextResponse } from "next/server";
-import { match } from "@formatjs/intl-localematcher";
-import Negotiator from "negotiator";
-
-// Define your supported locales
-const locales = ["es", "en"];
-const defaultLocale = "es";
-
-// Get the preferred locale from the request
-function getLocale(request) {
-  // For testing purposes you might want to override this
-  // return 'es'
-
-  const negotiatorHeaders = {};
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
-
-  // Use negotiator and intl-localematcher to get the best locale
-  const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
-  return match(languages, locales, defaultLocale);
-}
+import { handleI18nMiddleware } from "./middlewares/i18n.js";
 
 export function middleware(request) {
-  const pathname = request.nextUrl.pathname;
-
-  // Check if the pathname already has a locale
-  const pathnameHasLocale = locales.some(
-    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-  );
-
-  if (pathnameHasLocale) return;
-
-  // Redirect if there is no locale in the pathname
-  const locale = getLocale(request);
-  request.nextUrl.pathname = `/${locale}${pathname}`;
-
-  return NextResponse.redirect(request.nextUrl);
+  return handleI18nMiddleware(request);
 }
 
 export const config = {
-  matcher: [
-    // Skip all internal paths (_next)
-    "/((?!_next|api|_vercel|.*\\..*).*)",
-    // Optional: only run on root (/) URL
-    // '/'
-  ],
+  matcher: ["/((?!_next|api|_vercel|.*\\..*).*)"],
 };
---
+++ front/middlewares/i18n.js
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+
+const locales = ["es", "en"];
+const defaultLocale = "es";
+
+function getLocale(request) {
+  const negotiatorHeaders = {};
+  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
+
+  const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
+  return match(languages, locales, defaultLocale);
+}
+
+export function handleI18nMiddleware(request) {
+  const pathname = request.nextUrl.pathname;
+
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  );
+
+  if (pathnameHasLocale) return NextResponse.next();
+
+  const locale = getLocale(request);
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+
+  return NextResponse.redirect(request.nextUrl);
+}
