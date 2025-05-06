f102448a9a6cc875984658fe32bf804a161f01f7
martirale <hola@alemartir.com>
2025-05-06 01:14:11 -0600

    Hide admin menu

--- front/components/sidebar/sidebar.jsx
+++ front/components/sidebar/sidebar.jsx
@@ -191,17 +191,19 @@ export default function Sidebar() {
               <BookHeart className="w-5 h-5 mr-2" />
               {sidebar.favorites}
             </Link>
-            <Link
-              href={`/${currentLang}/settings`}
-              className={`flex items-center p-4 rounded-lg leading-none border ${
-                isSettings
-                  ? "border-sand bg-onix"
-                  : `border-blackamber hover:bg-onix ${hoverBorder}`
-              } transition-all duration-300`}
-            >
-              <Settings2 className="w-5 h-5 mr-2" />
-              {sidebar.settings}
-            </Link>
+            {isAdmin && (
+              <Link
+                href={`/${currentLang}/settings`}
+                className={`flex items-center p-4 rounded-lg leading-none border ${
+                  isSettings
+                    ? "border-sand bg-onix"
+                    : `border-blackamber hover:bg-onix ${hoverBorder}`
+                } transition-all duration-300`}
+              >
+                <Settings2 className="w-5 h-5 mr-2" />
+                {sidebar.settings}
+              </Link>
+            )}
           </nav>
         )}
         {/* FOOTER BUTTONS */}
