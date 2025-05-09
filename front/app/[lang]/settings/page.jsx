cbf814a954fcfebb6378714462b4cd973eb2d529
martirale <hola@alemartir.com>
2025-05-09 03:14:53 -0600

    Add misc sidebar

--- front/app/[lang]/settings/page.jsx
+++ front/app/[lang]/settings/page.jsx
@@ -1,12 +1,17 @@
 import { getDictionary } from "@/lib/i18n/serverDictionary";
+import SidebarMisc from "@/ui/SidebarMisc";
 
 export default async function SettingsPage({ params }) {
   const { lang = "es" } = await params;
   const intl = await getDictionary(lang);
 
   return (
-    <div className="p-4">
-      <h2>{intl.settings.title}</h2>
+    <div className="flex">
+      <SidebarMisc>
+        <h2 className="text-onix">{intl.settings.title}</h2>
+      </SidebarMisc>
+
+      <div className="w-full md:w-8/12 2xl:w-9/12 p-4"></div>
     </div>
   );
 }
