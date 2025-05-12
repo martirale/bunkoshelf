19fa96f3e56f4ee0e85d7c7aa4558ba887dafde4
martirale <hola@alemartir.com>
2025-05-12 01:52:26 -0600

    Add coming soon message

--- front/app/[lang]/books/page.jsx
+++ front/app/[lang]/books/page.jsx
@@ -1,15 +1,15 @@
 import { getDictionary } from "@/lib/i18n/serverDictionary";
-import {
-  LibraryBig,
-  BookMarked,
-  BookPlus,
-  BookDown,
-  BookCheck,
-} from "lucide-react";
+import { Construction } from "lucide-react";
+import { Loader2 } from "lucide-react";
 
 export default async function BooksPage({ params }) {
   const { lang = "es" } = await params;
   const intl = await getDictionary(lang);
 
-  return <></>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <Construction className="w-16 h-16" />
+      <h2>{intl.misc.coming}</h2>
+    </div>
+  );
 }
