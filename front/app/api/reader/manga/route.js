47c8cfa67c72969b62f6fbb3be29c53211201f8c
martirale <hola@alemartir.com>
2025-05-14 04:08:45 -0600

    Refactor Prisma and clean timeup

--- front/app/api/reader/manga/route.js
+++ front/app/api/reader/manga/route.js
@@ -1,13 +1,10 @@
 import AdmZip from "adm-zip";
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
 import fs from "fs/promises";
 import path from "path";
 import os from "os";
+import prisma from "@/lib/prisma"; // Ajusta esta ruta si tu singleton está en otro lugar
 
-const prisma = new PrismaClient();
-
-// Extensiones válidas de imagen
 const validImageExtensions = [".jpg", ".jpeg", ".png", ".gif", ".webp"];
 
 export async function POST(req) {
@@ -34,9 +31,7 @@ export async function POST(req) {
     const entries = zipEntries
       .filter((entry) => {
         const ext = path.extname(entry.entryName).toLowerCase();
-        return (
-          !entry.isDirectory && validImageExtensions.includes(ext) // solo imágenes válidas
-        );
+        return !entry.isDirectory && validImageExtensions.includes(ext);
       })
       .sort((a, b) =>
         a.entryName.localeCompare(b.entryName, undefined, { numeric: true })
@@ -47,22 +42,21 @@ export async function POST(req) {
     setTimeout(async () => {
       try {
         await fs.rm(tempDir, { recursive: true, force: true });
-        console.log("🧹 Temp folder deleted:", tempDir);
+        console.log("Temp folder deleted:", tempDir);
       } catch (err) {
-        console.error("⚠️ Failed to delete temp folder:", tempDir, err);
+        console.error("Failed to delete temp folder:", tempDir, err);
       }
-    }, 15 * 60 * 1000); // 15 minutos
+    }, 60 * 60 * 1000); // 1 hora
 
-    const writePromises = entries.map(async (entry) => {
-      const imagePath = path.join(tempDir, path.basename(entry.entryName));
-      const fileData = entry.getData();
-      await fs.writeFile(imagePath, fileData);
+    const imagePaths = await Promise.all(
+      entries.map(async (entry) => {
+        const imagePath = path.join(tempDir, path.basename(entry.entryName));
+        const fileData = entry.getData();
+        await fs.writeFile(imagePath, fileData);
 
-      // URL accesible vía API
-      return `/api/reader/tempImage?path=${encodeURIComponent(imagePath)}`;
-    });
-
-    const imagePaths = await Promise.all(writePromises);
+        return `/api/reader/tempImage?path=${encodeURIComponent(imagePath)}`;
+      })
+    );
 
     return NextResponse.json({ images: imagePaths });
   } catch (err) {
