8d5de1bf5f3fe155f88e71340c76b566c7f9e2d2
martirale <hola@alemartir.com>
2025-05-13 22:33:03 -0600

    Remove unused icons

--- front/app/[lang]/page.jsx
+++ front/app/[lang]/page.jsx
@@ -1,11 +1,4 @@
 import { getDictionary } from "@/lib/i18n/serverDictionary";
-import {
-  LibraryBig,
-  BookMarked,
-  BookPlus,
-  BookDown,
-  BookCheck,
-} from "lucide-react";
 
 export default async function HomePage({ params }) {
   const { lang = "es" } = await params;
