4644741126775018da6e2e8a5f8aee8889301506
martirale <hola@alemartir.com>
2025-05-15 17:21:01 -0600

    Adjust gap in nav

--- front/components/library/manga/MangaNav.jsx
+++ front/components/library/manga/MangaNav.jsx
@@ -13,7 +13,7 @@ export default function MangaNav({ lang, intl }) {
   ];
 
   return (
-    <nav className="flex justify-center pt-8 pb-4 px-4 gap-2 bg-lilah">
+    <nav className="flex justify-center pt-8 pb-4 px-4 gap-2 md:gap-4 bg-lilah">
       {links.map(({ label, href }) => {
         const isActive = pathname === href;
         return (
