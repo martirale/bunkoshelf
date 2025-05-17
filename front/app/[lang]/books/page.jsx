e9aac3da46205bf9a7678b23c6e873f98eef0e1c
martirale <hola@alemartir.com>
2025-05-17 00:16:10 -0600

    Adjust paddings

--- front/app/[lang]/books/page.jsx
+++ front/app/[lang]/books/page.jsx
@@ -1,15 +1,14 @@
 import { getDictionary } from "@/lib/i18n/serverDictionary";
 import { Construction } from "lucide-react";
-import { Loader2 } from "lucide-react";
 
 export default async function BooksPage({ params }) {
   const { lang = "es" } = await params;
   const intl = await getDictionary(lang);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-4">
+    <div className="flex flex-col items-center justify-center h-screen gap-4 p-4">
       <Construction className="w-16 h-16" />
-      <h2>{intl.misc.coming}</h2>
+      <h2 className="text-center">{intl.misc.coming}</h2>
     </div>
   );
 }
