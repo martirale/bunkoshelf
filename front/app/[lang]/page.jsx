23ddc6b137d17bc3dae26bb2592d9bd2188853c3
martirale <hola@alemartir.com>
2025-05-18 03:00:19 -0600

    Add develop warning

--- front/app/[lang]/page.jsx
+++ front/app/[lang]/page.jsx
@@ -1,8 +1,17 @@
 import { getDictionary } from "@/lib/i18n/serverDictionary";
+import AlertBox from "@/ui/AlertBox";
 
 export default async function HomePage({ params }) {
   const { lang = "es" } = await params;
   const intl = await getDictionary(lang);
 
-  return <></>;
+  return (
+    <div className="p-4">
+      <AlertBox
+        title={intl.toastDev.appDevelopTt}
+        description={intl.toastDev.appDevelop}
+        variant="warning"
+      />
+    </div>
+  );
 }
