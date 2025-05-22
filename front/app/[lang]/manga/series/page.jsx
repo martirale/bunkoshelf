2bc501a7b45503197cb9b0f9752c80f82eab989d
martirale <hola@alemartir.com>
2025-05-22 00:23:23 -0600

    Fix search params promise

--- front/app/[lang]/manga/series/page.jsx
+++ front/app/[lang]/manga/series/page.jsx
@@ -1,8 +1,14 @@
 import { getDictionary } from "@/lib/i18n/Dictionary";
 import LibraryGridSeries from "@/components/library/manga/grid/LibraryGridSeries";
 
-export default async function MangaSeriesPage({ searchParams, params }) {
-  const page = parseInt(searchParams.page || "1", 10);
+export default async function MangaSeriesPage({
+  searchParams: _searchParams,
+  params,
+}) {
+  const searchParams = await _searchParams;
+  const { page: pageRaw = "1" } = searchParams;
+  const page = parseInt(pageRaw, 10);
+
   const { lang = "es" } = await params;
   const intl = await getDictionary(lang);
 
--- front/app/[lang]/manga/volumes/page.jsx
+++ front/app/[lang]/manga/volumes/page.jsx
@@ -1,8 +1,14 @@
 import { getDictionary } from "@/lib/i18n/Dictionary";
 import LibraryGridVolumes from "@/components/library/manga/grid/LibraryGridVolumes";
 
-export default async function MangaVolumesPage({ searchParams, params }) {
-  const page = parseInt(searchParams.page || "1", 10);
+export default async function MangaVolumesPage({
+  searchParams: _searchParams,
+  params,
+}) {
+  const searchParams = await _searchParams;
+  const { page: pageRaw = "1" } = searchParams;
+  const page = parseInt(pageRaw, 10);
+
   const { lang = "es" } = await params;
   const intl = await getDictionary(lang);
 
