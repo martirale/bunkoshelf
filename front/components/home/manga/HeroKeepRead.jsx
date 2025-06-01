ea6fde593787fa6da9eb576e0be0b67cb02aa2f1
martirale <hola@alemartir.com>
2025-06-01 12:25:24 -0600

    Add library link in keep read title

--- front/components/home/manga/HeroKeepRead.jsx
+++ front/components/home/manga/HeroKeepRead.jsx
@@ -1,8 +1,9 @@
-import { LibraryBig } from "lucide-react";
+import { LibraryBig, ChevronRight } from "lucide-react";
 import MangaCard from "@/ui/library/manga/MangaCard";
 import prisma from "@/lib/prisma";
 import { verifySession } from "@/lib/auth/verifySession";
 import { sortByPaddedTitle } from "@/lib/utils";
+import Link from "next/link";
 
 export default async function HeroKeepRead({ lang, intl }) {
   const currentUser = await verifySession();
@@ -58,10 +59,13 @@ export default async function HeroKeepRead({ lang, intl }) {
   return (
     <div className="flex-shrink-0 w-full md:w-1/1 2xl:w-3/5">
       <div className="flex justify-between items-center mb-4">
-        <h2 className="text-onix flex items-center text-base md:text-lg">
-          <LibraryBig className="w-6 h-6 md:w-7 md:h-7 mr-2" />
-          {intl.libraries.keepReading}
-        </h2>
+        <Link href={`/${lang}/manga`}>
+          <h2 className="text-onix flex items-center text-base md:text-lg group">
+            <LibraryBig className="w-6 h-6 md:w-7 md:h-7 mr-2" />
+            {intl.libraries.keepReading}
+            <ChevronRight className="w-6 h-6 md:w-7 md:h-7 ml-2 text-pearl group-hover:text-onix transition-all duration-300" />
+          </h2>
+        </Link>
       </div>
 
       <div className="w-full px-12 md:px-0">
