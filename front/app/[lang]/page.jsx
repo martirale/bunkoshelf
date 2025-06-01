79e3ece153e646c860c809da519054229349e249
martirale <hola@alemartir.com>
2025-06-01 12:42:20 -0600

    Add link to profile

--- front/app/[lang]/page.jsx
+++ front/app/[lang]/page.jsx
@@ -2,6 +2,8 @@ import HeroKeepRead from "@/components/home/manga/HeroKeepRead";
 import RowNewVols from "@/components/home/manga/RowNewVols";
 import ReaderStatsPanel from "@/components/stats/ReaderPanel";
 import { getDictionary } from "@/lib/i18n/Dictionary";
+import { ChevronRight } from "lucide-react";
+import Link from "next/link";
 
 export default async function HomePage({ params }) {
   const { lang = "es" } = await params;
@@ -15,7 +17,18 @@ export default async function HomePage({ params }) {
         </div>
 
         <div className="w-full md:w-1/2 flex flex-col justify-between mt-8 md:mt-11">
-          <ReaderStatsPanel lang={lang} intl={intl} mdCols="md:grid-cols-3" />
+          <div className="group">
+            <ReaderStatsPanel lang={lang} intl={intl} mdCols="md:grid-cols-3" />
+            <div className="flex justify-end mt-2">
+              <Link
+                href={`/${lang}/profile`}
+                className="w-max flex items-center text-base text-pearl group-hover:text-onix hover:underline transition-all duration-300"
+              >
+                Ir al perfil
+                <ChevronRight className="w-5 h-5 ml-1" />
+              </Link>
+            </div>
+          </div>
           <RowNewVols lang={lang} intl={intl} />
         </div>
       </div>
