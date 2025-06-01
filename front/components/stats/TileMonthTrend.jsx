39f1d5a60016aad8632d2c6c74ca2cdb361394d9
martirale <hola@alemartir.com>
2025-06-01 04:28:55 -0600

    Fix icon size

--- front/components/stats/TileMonthTrend.jsx
+++ front/components/stats/TileMonthTrend.jsx
@@ -25,9 +25,11 @@ export default function TileMonthTrend({
     >
       <span className={`${textColor} text-sm uppercase`}>{title}</span>
       <div
-        className={`font-boldonse ${textColor} 2xl:text-2xl leading-7.5 mt-2 flex items-center gap-2`}
+        className={`font-boldonse ${textColor} 2xl:text-2xl leading-7.5 mt-2 flex items-center`}
       >
-        {percentageChange}%<IconComponent className={`w-5 h-5 ${color}`} />
+        {percentageChange}
+        <span className="text-sm">%</span>
+        <IconComponent className={`w-5 h-5 ${color}`} />
       </div>
     </div>
   );
