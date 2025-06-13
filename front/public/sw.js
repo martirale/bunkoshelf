8e4843faa2cbe58d34b057343e4655aba435cd90
martirale <hola@alemartir.com>
2025-06-13 14:21:50 -0600

    Create service worker

---
+++ front/public/sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("push", (event) => {
+  const data = event.data?.json() ?? {};
+  const title = data.title || "Notificación";
+  const options = {
+    body: data.body || "Tienes una nueva notificación.",
+    icon: "/pwa/bunkoshelf-icon-192.png",
+    badge: "/pwa/bunkoshelf-icon-192.png",
+    data: data.url || "/",
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow(event.notification.data));
+});
