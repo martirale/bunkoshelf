234c24d1705b2105dd3aab0c0e97a81642fc0ad1
martirale <hola@alemartir.com>
2025-06-13 20:59:29 -0600

    Remove web push

--- front/components/stats/TileStreak.jsx
+++ front/components/stats/TileStreak.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-export default function TileStreak({ title, lang, intl, bgColor, textColor }) {
+export default function TileStreak({ title, intl, bgColor, textColor }) {
   const [streak, setStreak] = useState("—");
 
   useEffect(() => {
@@ -38,55 +38,6 @@ export default function TileStreak({ title, lang, intl, bgColor, textColor }) {
         }
 
         setStreak(count.toString());
-
-        // Notificación de racha
-        const nowHour = now.getHours();
-        const nowMinutes = now.getMinutes();
-
-        const todayStr = now.toISOString().split("T")[0];
-        const lastNotified = localStorage.getItem("lastStreakNotify");
-
-        const yesterdayDate = new Date(now);
-        yesterdayDate.setDate(now.getDate() - 1);
-        const yYear = yesterdayDate.getFullYear();
-        const yMonth = String(yesterdayDate.getMonth() + 1).padStart(2, "0");
-        const yDay = String(yesterdayDate.getDate()).padStart(2, "0");
-        const yesterday = `${yYear}-${yMonth}-${yDay}`;
-
-        const readYesterday = readDaySet.has(yesterday);
-        const readToday = readDaySet.has(todayStr);
-
-        const isAfter2030 =
-          nowHour > 20 || (nowHour === 20 && nowMinutes >= 30);
-
-        if (
-          isAfter2030 &&
-          readYesterday &&
-          !readToday &&
-          lastNotified !== todayStr
-        ) {
-          const registration = await navigator.serviceWorker.ready;
-          const subscription = await registration.pushManager.getSubscription();
-
-          if (subscription) {
-            await fetch("https://push.amlab.site/send", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                subscription,
-                payload: {
-                  title: intl.push.ttReadStreak,
-                  body: intl.push.bodyReadStreak,
-                  url: `/${lang}/manga`,
-                },
-              }),
-            });
-
-            localStorage.setItem("lastStreakNotify", todayStr);
-          }
-        }
       } catch (error) {
         console.error("Error al calcular la racha de lectura:", error);
         setStreak("—");
@@ -94,7 +45,7 @@ export default function TileStreak({ title, lang, intl, bgColor, textColor }) {
     }
 
     fetchData();
-  }, [intl, lang]);
+  }, []);
 
   return (
     <div
