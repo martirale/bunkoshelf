097184f29faea011ac32d67175bd3235ff13703e
martirale <hola@alemartir.com>
2025-06-16 20:41:26 -0600

    Refactor profile nav

--- front/components/profile/ProfileNav.jsx
+++ front/components/profile/ProfileNav.jsx
@@ -6,44 +6,42 @@ import { usePathname, useParams } from "next/navigation";
 import clsx from "clsx";
 
 export default function ProfileNav({ intl }) {
-  // Lang options
   const params = useParams();
   const pathname = usePathname();
   const currentLang = params.lang || "es";
 
-  // Check current routes
-  const isProfile = pathname === `/${currentLang}/profile`;
-  const isProfileUpdate = pathname.startsWith(`/${currentLang}/profile/update`);
+  const links = [
+    {
+      label: intl.profile.overview,
+      href: `/${currentLang}/profile`,
+      icon: Bolt,
+      isActive: pathname === `/${currentLang}/profile`,
+    },
+    {
+      label: intl.profile.updateProfile,
+      href: `/${currentLang}/profile/update`,
+      icon: UserRoundPen,
+      isActive: pathname === `/${currentLang}/profile/update`,
+    },
+  ];
 
   return (
-    <>
-      <div className="mt-4 md:mt-16">
-        <div className={clsx("md:space-y-2", "flex md:block gap-2")}>
+    <div className="mt-4 md:mt-16">
+      <div className={clsx("md:space-y-2", "flex md:block gap-2")}>
+        {links.map(({ href, icon: Icon, label, isActive }, index) => (
           <Link
-            href={`/${currentLang}/profile`}
+            key={index}
+            href={href}
             className={clsx(
               "flex flex-col md:flex-row justify-center md:justify-start w-full items-center p-4 rounded-lg leading-none text-onix transition-all duration-300",
-              isProfile ? "bg-sand" : "hover:bg-sand"
+              isActive ? "bg-sand" : "hover:bg-sand"
             )}
           >
-            <Bolt className="w-5 h-5 md:mr-2" />
-            <span className="hidden md:inline">{intl.settings.overview}</span>
+            <Icon className="w-5 h-5 mr-1 md:mr-2" />
+            <span className="hidden md:inline">{label}</span>
           </Link>
-
-          <Link
-            href={`/${currentLang}/profile/update`}
-            className={clsx(
-              "flex flex-col md:flex-row justify-center md:justify-start w-full items-center p-4 rounded-lg leading-none text-onix transition-all duration-300",
-              isProfileUpdate ? "bg-sand" : "hover:bg-sand"
-            )}
-          >
-            <UserRoundPen className="w-5 h-5 md:mr-2" />
-            <span className="hidden md:inline">
-              {intl.profile.updateProfile}
-            </span>
-          </Link>
-        </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
