102855699867391bf2aaa7a4c2343ba2af55874d
martirale <hola@alemartir.com>
2025-11-24 12:56:54 -0600

    Adjust home bg color

--- app/[lang]/page.jsx
+++ app/[lang]/page.jsx
@@ -14,43 +14,46 @@ export default async function HomePage({ params }) {
 
   return (
     <>
-      <div
-        className={clsx(
-          "bg-pearl flex flex-col p-4 mb-24 gap-4",
-          "md:flex-row md:mb-0"
-        )}
-      >
-        <div className="w-full md:w-1/2">
-          <HeroKeepRead lang={lang} intl={intl} />
-        </div>
-
-        <div className="w-full md:w-1/2 flex flex-col justify-between">
-          <div className="group">
-            <div className="hidden md:flex justify-end gap-2">
-              <PushButton lang={lang} intl={intl} />
-              <ReloadButton />
-            </div>
-
-            <ReaderStatsPanel
-              lang={lang}
-              intl={intl}
-              mdCols="md:grid-cols-3 mt-4"
-            />
-            <div className="flex justify-center ml-4 mt-2 md:justify-end md:ml-0">
-              <Link
-                href={`/${lang}/profile`}
-                className={clsx(
-                  "text-onix w-max flex items-center text-base",
-                  "hover:underline transition-all duration-300"
-                )}
-              >
-                {intl.home.goToProfile}
-                <ChevronRightIcon size={20} className="ml-1" />
-              </Link>
-            </div>
+      <div className="relative">
+        <div className="fixed inset-0 -z-10 bg-pearl" />
+        <div
+          className={clsx(
+            "bg-pearl flex flex-col p-4 mb-24 gap-4",
+            "md:flex-row md:mb-0"
+          )}
+        >
+          <div className="w-full md:w-1/2">
+            <HeroKeepRead lang={lang} intl={intl} />
           </div>
 
-          <RowNewVols lang={lang} intl={intl} />
+          <div className="w-full md:w-1/2 flex flex-col justify-between">
+            <div className="group">
+              <div className="hidden md:flex justify-end gap-2">
+                <PushButton lang={lang} intl={intl} />
+                <ReloadButton />
+              </div>
+
+              <ReaderStatsPanel
+                lang={lang}
+                intl={intl}
+                mdCols="md:grid-cols-3 mt-4"
+              />
+              <div className="flex justify-center ml-4 mt-2 md:justify-end md:ml-0">
+                <Link
+                  href={`/${lang}/profile`}
+                  className={clsx(
+                    "text-onix w-max flex items-center text-base",
+                    "hover:underline transition-all duration-300"
+                  )}
+                >
+                  {intl.home.goToProfile}
+                  <ChevronRightIcon size={20} className="ml-1" />
+                </Link>
+              </div>
+            </div>
+
+            <RowNewVols lang={lang} intl={intl} />
+          </div>
         </div>
       </div>
     </>
