0fe37254cc14b6833d9726622f8621e2c3c1c4c9
martirale <hola@alemartir.com>
2026-09-11 17:59:57 -0600

    Add book carousel keys

--- src/components/library/books/BookOverview.tsx
+++ src/components/library/books/BookOverview.tsx
@@ -11,7 +11,7 @@ export default async function BookOverview({ lang, intl }: BookOverviewProps) {
   if (!books.length) return <p className="p-4 text-center">No hay libros indexados todavía.</p>;
   const recentlyRead = user ? await listRecentlyReadBooks(user.id, 12) : [];
   return <div className="library-overview p-4">
-    <div className="library-overview-section"><BookRowCarousel books={books.slice(0, 12)} lang={lang} intl={intl} header={<h2 className="flex items-center text-base md:text-lg"><BookPlusIcon size={28} className="mr-2" />{intl.libraries.recentlyAdded as string}</h2>} /></div>
-    {recentlyRead.length > 0 && <div className="library-overview-section"><BookRowCarousel books={recentlyRead} lang={lang} intl={intl} header={<h2 className="flex items-center text-base md:text-lg"><BookCheckIcon size={28} className="mr-2" />{intl.libraries.recentlyRead as string}</h2>} /></div>}
+    <div className="library-overview-section"><BookRowCarousel books={books.slice(0, 12)} lang={lang} intl={intl} header={<h2 key="header" className="flex items-center text-base md:text-lg"><BookPlusIcon size={28} className="mr-2" />{intl.libraries.recentlyAdded as string}</h2>} /></div>
+    {recentlyRead.length > 0 && <div className="library-overview-section"><BookRowCarousel books={recentlyRead} lang={lang} intl={intl} header={<h2 key="header" className="flex items-center text-base md:text-lg"><BookCheckIcon size={28} className="mr-2" />{intl.libraries.recentlyRead as string}</h2>} /></div>}
   </div>;
 }
